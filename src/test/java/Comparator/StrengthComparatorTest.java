ca4f20c90b3c5b7cd986e70a68c057fab3fe642d
Masih <said0983@stud.kea.dk>
2022-11-10 23:11:40 +0100

    Added some unit tests

--- src/test/java/Comparator/StrengthComparatorTest.java
+++ src/test/java/Comparator/StrengthComparatorTest.java
@@ -1,6 +1,11 @@
 package Comparator;
 
+import Controller.Controller;
+import Superhero.Superhero;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +13,14 @@ class StrengthComparatorTest {
 
     @Test
     void compare() {
+        Controller controller = new Controller();
+        Superhero s1 = new Superhero("ole", true, "fisker", 1930, 10);
+        Superhero s2 = new Superhero("batman", true, "bat powers", 1986, 100);
+        Superhero s3 = new Superhero("spiderman", true, "spider powers", 2005, 50);
+        Superhero s4 = new Superhero("bjorn", true, "advokat", 1990, 5);
+        ArrayList<Superhero> actual = new ArrayList<Superhero>(Arrays.asList(s1, s2, s3, s4));
+        ArrayList<Superhero> expected = new ArrayList<Superhero>(Arrays.asList(s2, s3, s1, s4)); //Den rigtige rækkefølge
+        //Sorterer array list "actual" ifølge vores comparator som så burde give den samme rækkefølge som array list "expected"
+        assertEquals(controller.sortSuperStrength(actual), expected);
     }
 }
--- src/test/java/Comparator/SuperPowerComparatorTest.java
+++ src/test/java/Comparator/SuperPowerComparatorTest.java
@@ -1,6 +1,11 @@
 package Comparator;
 
+import Controller.Controller;
+import Superhero.Superhero;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +13,14 @@ class SuperPowerComparatorTest {
 
     @Test
     void compare() {
+        Controller controller = new Controller();
+        Superhero s1 = new Superhero("ole", true, "fisker", 1930, 10);
+        Superhero s2 = new Superhero("batman", true, "bat powers", 1986, 100);
+        Superhero s3 = new Superhero("spiderman", true, "spider powers", 2005, 50);
+        Superhero s4 = new Superhero("bjorn", true, "advokat", 1990, 5);
+        ArrayList<Superhero> actual = new ArrayList<Superhero>(Arrays.asList(s1, s2, s3, s4));
+        ArrayList<Superhero> expected = new ArrayList<Superhero>(Arrays.asList(s4, s2, s1, s3)); //Den rigtige rækkefølge
+        //Sorterer array list "actual" ifølge vores comparator som så burde give den samme rækkefølge som array list "expected"
+        assertEquals(controller.sortSuperPower(actual), expected);
     }
 }
