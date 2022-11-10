b82651bb2e09e8ecc9b155c677a389733d2797e6
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-11-10 17:17:06 +0100

    Junit

---
+++ src/test/java/Comparator/IsHumanComparatorTest.java
@@ -0,0 +1,12 @@
+package Comparator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsHumanComparatorTest {
+
+    @Test
+    void compare() {
+    }
+}
---
+++ src/test/java/Comparator/NameComparatorTest.java
@@ -0,0 +1,12 @@
+package Comparator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NameComparatorTest {
+
+    @Test
+    void compare() {
+    }
+}
