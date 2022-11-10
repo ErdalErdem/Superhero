8cbcaee46e1ac6181502aa479612dda6a02889dc
Zana0046@stud.kea.dk <90389855+Dannicolak@users.noreply.github.com>
2022-11-10 22:09:10 +0100

    Junit

---
+++ src/test/java/Controller/ControllerTest.java
@@ -0,0 +1,16 @@
+package Controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void readData() {
+    }
+
+    @Test
+    void saveData() {
+    }
+}
